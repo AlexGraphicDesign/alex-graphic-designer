7697a5aaa3f09b24151c3190a7a050e74cfcde5b
alexgraphicdesigner <adenys.ad@gmail.com>
2019-06-02 18:10:18 +0200

    travail bandeau cookie

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -4,14 +4,12 @@ function scrolled(){
   //On récupère la valeur du scroll
   var scrollVal = $(document).scrollTop();
 
-  if (scrollVal > 70){
+  if (scrollVal > 50){
     $('#header').addClass("fixed-top");
   }else{
     $('#header').removeClass("fixed-top");
   }
-
 }
-
 
 
 jQuery(document).ready(function($){
@@ -23,18 +21,31 @@ jQuery(document).ready(function($){
       scrolled();
   });
 
-  //gestion bandeau cookies
+  //Gestion des cookies
   if ($.cookie('cookieblock') === undefined)  {
 
     //On affiche le bloc
-    $('#cookie').fadeIn(300);
+    $('#cookies').fadeIn(300);
 
-    //masquer le bloc
+    //masquer le bloc = j'accepte les cookies
     $('#cookie-btn-ok').click(function(e){
       e.preventDefault();
-      $('#cookie').fadeOut();
+      $('#cookies').fadeOut(300);
       $.cookie('cookieblock', 'viewed', {expires: 30 * 12});
     })
+
+    //masquer le bloc = je refuse les cookies
+    $('#cookie-btn-ko').click(function(e){
+      e.preventDefault();
+      $('#cookies').fadeOut();
+      $.cookie('cookieblock', 'viewed', {expires: 30 * 12});
+      $.cookie('cookiecancel', '1', {expires: 30 * 12});
+    })
+
+  }
+
+  if ($.cookie('cookiecancel') === undefined)  {
+    //Code analytics
   }
 
 });
