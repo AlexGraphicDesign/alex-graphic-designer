ae322171cd5f5167db92e148f6ce5f9618c9e564
Alexandre MCA <alexandre@mcapatrimoine.com>
2020-02-01 20:09:14 +0100

    maj style + gulp

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,8 @@ const {src, dest, task, series, parallel, watch} = require("gulp");
 const sass = require("gulp-sass"),
       del = require("del"),
       plumber = require("gulp-plumber"),
-      cssautoprefixer = require("gulp-autoprefixer"),
+      autoprefixer = require("autoprefixer"),
+      postcss = require('gulp-postcss'),
       cssbeautify = require("gulp-cssbeautify"),
       cleanCSS = require ("gulp-clean-css"),
       concat = require("gulp-concat"),
@@ -18,10 +19,8 @@ const sass = require("gulp-sass"),
 
 //Chemins fichiers sources et de destination
 const paths = {
-      srcBootstrap : 'node_modules/bootstrap/scss/*.scss',
-      srcBootstrapComposants : 'node_modules/bootstrap/scss/**/*.scss',
-      srcCustomCss : 'src/scss/*.scss',
-      srcCustomComposantsCss : 'src/scss/**/*.scss',
+      srcCss : 'src/scss/*.scss',
+      srcComposantsCss : 'src/scss/**/*.scss',
 
       srcJs: 'src/js/*.js',
       srcBootstrapJs: 'node_modules/bootstrap/dist/js/*.js',
@@ -54,16 +53,15 @@ function gulpReload(done){
 
 //Compilation du SASS de developpement
 function gulpSass(done){
-  return src(paths.srcCustomCss, {sourcemaps: true})
+  return src(paths.srcCss, {sourcemaps: true})
   .pipe(plumber())
   .pipe(sass({
     errLogToConsole: true
   }))
   .on( 'error', console.error.bind( console ) )
-  .pipe(cssautoprefixer({
-          browsers: ['> 0%']
-   }))
+  .pipe(postcss([ autoprefixer() ]))
   .pipe(cssbeautify({indent: '  '})) //indentation de deux espaces
+  .pipe(cleanCSS({compatibility: 'ie8'}))
   .pipe(dest(paths.distCss, {sourcemaps: '.'}))
   .pipe(browserSync.stream());
   done();
@@ -81,7 +79,7 @@ function gulpJS(done){
 
 //On écoute les modifications de fichiers pour rechargement de la page automatique
 function gulpWatch(){
-  watch([paths.srcBootstrap, paths.srcBootstrapComposants, paths.srcCustomCss, paths.srcCustomComposantsCss], series(gulpSass, gulpReload));
+  watch([paths.srcCss, paths.srcComposantsCss], series(gulpSass, gulpReload));
   watch([paths.srcBootstrapJs, paths.srcJs], series(gulpJS, gulpReload));
 }
 
