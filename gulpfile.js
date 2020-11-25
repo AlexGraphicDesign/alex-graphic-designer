87669c76ef09c280a45b6212ec641b43422ef31a
alexandre_mca <alexandre@mcapatrimoine.com>
2020-11-25 22:59:02 +0100

    dev

--- gulpfile.js
+++ gulpfile.js
@@ -9,11 +9,12 @@ const {src, dest, task, series, parallel, watch, lastRun} = require("gulp");
 const sass = require("gulp-sass"),
       del = require("del"),
       plumber = require("gulp-plumber"),
-      autoprefixer = require("autoprefixer"),
-      postcss = require('gulp-postcss'),
-      cssbeautify = require("gulp-cssbeautify"),
-      cleanCSS = require ("gulp-clean-css"),
+      rename = require("gulp-rename"),
+      uglify = require("gulp-uglify"),
+      cleancss = require ("gulp-clean-css"),
+      imagemin = require("gulp-imagemin"),
       concat = require("gulp-concat"),
+      sourcemap = require("gulp-sourcemaps"),
       browserSync = require("browser-sync").create();
 
 
@@ -27,6 +28,13 @@ const paths = {
   css: {
       src: ['src/scss/*.scss', 'src/scss/**/*.scss'],
       dest: './css'
+  },
+  images: {
+      src: ['./src/img/**/*'],
+      dest: './images'
+  },
+  twig:{
+    src: ['./templates']
   }
 
 }
@@ -36,7 +44,7 @@ const paths = {
 //----------------------------------------------------------------
 
 function clean(){
-    return del([paths.css.dest, paths.js.dest])
+    return del([paths.css.dest, paths.js.dest, paths.images.dest])
 }
 
 //BrowserSync, création d'un serveur local
@@ -55,37 +63,48 @@ function gulpReload(done){
 
 //Compilation du SASS de developpement
 function gulpStyle(done){
-  //return src([paths.srcStyleCss, paths.srcBootstrapCss], {sourcemaps: true})
-  return src(paths.css.src, {sourcemaps: true})
-  .pipe(plumber())
-  .pipe(sass({
-    errLogToConsole: true
-  }))
-  .on( 'error', console.error.bind( console ) )
-  .pipe(postcss([ autoprefixer() ]))
-  .pipe(cssbeautify({indent: '  '})) //indentation de deux espaces
-  .pipe(cleanCSS({compatibility: 'ie8'}))
-  .pipe(dest(paths.css.dest, {sourcemaps: '.'}))
-  .pipe(browserSync.stream());
-  done();
+  return src(paths.css.src)
+    .pipe(plumber())
+    .pipe(sourcemap.init({loadMaps: true}))
+    .pipe(sass().on('error', sass.logError))
+    .pipe(cleancss())
+    .pipe(rename({ suffix: ".min" }))
+    .pipe(sourcemap.write('./'))
+    .pipe(dest(paths.css.dest))
+    .pipe(browserSync.stream())
+    done();
 }
 
 //Compilation du Javascript de developpement
 function gulpJS(done){
   return src(paths.js.src)
-  .pipe(plumber())
-  .pipe(concat('scripts.js'))
-  .pipe(dest(paths.js.dest))
-  .pipe(browserSync.stream() );
-  done();
+    .pipe(plumber())
+    .pipe(sourcemap.init())
+    .pipe(uglify())
+    .pipe(concat('scripts.js'))
+    .pipe(rename({ suffix: ".min" }))
+    .pipe(sourcemap.write())
+    .pipe(dest(paths.js.dest))
+    .pipe(browserSync.stream())
+    done();
+}
+
+//Minification des images
+function gulpImg(done){
+    return src(paths.images.src, {since: lastRun(gulpImg)} )
+    .pipe(imagemin())
+    .pipe(dest(paths.images.dest))
+    done();
 }
 
 //On écoute les modifications de fichiers pour rechargement de la page automatique
 function gulpWatch(){
+  watch(paths.images.src, series(gulpImg, gulpReload));
   watch(paths.css.src, series(gulpStyle, gulpReload));
   watch(paths.js.src, series(gulpJS, gulpReload));
+  watch(paths.twig.src, gulpReload);
 }
 
 //On liste les tâches
 exports.buildDev = parallel(gulpWatch, gulpBrowserSync);
-exports.default = series(clean, parallel(gulpStyle, gulpJS));
+exports.default = series(clean, parallel(gulpStyle, gulpJS, gulpImg));
