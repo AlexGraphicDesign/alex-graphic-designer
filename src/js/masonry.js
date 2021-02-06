de882fbbe4cd221690e5b3d0cdf0d7916738c8d9
AlexGraphicDesign <adenys.ad@gmail.com>
2021-02-06 13:21:19 +0100

    dev masonry chargement des images

--- src/js/masonry.js
+++ src/js/masonry.js
@@ -3,35 +3,46 @@
 jQuery(document).ready(function($){
 
   resizeAllMasonryItems();
+  waitForImages();
 
   //On appelle la/les fonction(s) qui doit s'éxécuter au resize
   $(window).resize(function(e) {
+    resizeAllMasonryItems();
   });
 
 });
 
 
 //Masonry
+
 //Calcul de la hauteur de chaque tuile
 function resizeMasonryItem(item){
-
   var grid = $('.masonry-container'),
       rowGap = parseInt(grid.css('grid-row-gap')),
       rowHeight = parseInt(grid.css('grid-auto-rows')),
-      itemMasonry = $(item).find('.blog__item').get(0);
+      itemMasonry = $(item).find('.blog__item');
 
-  var rowSpan = Math.ceil((itemMasonry.getBoundingClientRect().height+rowGap)/(rowHeight+rowGap));
-
+  var rowSpan = Math.ceil((itemMasonry.height()+rowGap)/(rowHeight+rowGap));
   item.style.gridRowEnd = 'span '+ rowSpan;
-
 }
 
 
-function resizeAllMasonryItems(){
+//On charge la fonction resizeMasonryItem() quand toutes les images sont chargées
+function waitForImages(){
+  var allItems = $('.masonry-item');
+  for(var i=0;i<allItems.length;i++){
+    imagesLoaded( allItems[i], function(instance) {
+      var item = instance.elements[0];
+      resizeMasonryItem(item);
+    });
+  }
+}
 
+
+//On redimensionne tous les items
+function resizeAllMasonryItems(){
   var allItems = $('.masonry-item');
   for(var i=0;i<allItems.length;i++){
     resizeMasonryItem(allItems[i]);
   }
-
 }
